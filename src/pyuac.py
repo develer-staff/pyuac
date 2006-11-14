43ec34283350b2ca095425bb7af03f0de373f329
Matteo Bertini <naufraghi@develer.com>
2006-11-14 17:53:51 +0000

    Fattorizzate un paio di metodi, ora la GUI funziona in lettura, ma ancora va risistemata la logica interna.
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@11409 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/pyuac.py
+++ src/pyuac.py
@@ -9,65 +9,75 @@
 # Author: Matteo Bertini <naufraghi@develer.com>
 
 import sys
-from PyQt4 import QtCore, QtGui, uic
+from PyQt4 import uic
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 import libRemoteTimereg
 
-class TimeregApplication(QtGui.QApplication):
+class TimeregApplication(QApplication):
     def __init__(self, args):
-        QtGui.QApplication.__init__(self, args)
+        QApplication.__init__(self, args)
         win = TimeregWindow()
         win.show()
         sys.exit(self.exec_())
         
-class TimeregWindow(QtGui.QMainWindow):
+class TimeregWindow(QMainWindow):
     def __init__(self,):
-        QtGui.QMainWindow.__init__(self)
+        QMainWindow.__init__(self)
         self.ui = uic.loadUi("pyuac_edit.ui", self)
         self.rt = libRemoteTimereg.RemoteTimereg("http://www.develer.com/~naufraghi/achievo/", "matteo", "matteo99")
-        self._smartquery = ""
+
+        self._projphaseact = ""
+        self._hours = ""
+        self._comment = ""
+        self.projects = []
+        
         self._connectSlots()
-        self._setup_gui()
-    def _setup_gui(self):
-        self.ui.dateTimeregDate.setDate(QtCore.QDate.currentDate())
+        self._setupGui()
+
+    def _setupGui(self):
+        self.ui.dateTimeregDate.setDate(QDate.currentDate())
         self._slotSmartQueryChanged()
+
     def _connectSlots(self):
-        self.connect(self.ui.comboSmartQuery, QtCore.SIGNAL("editTextChanged(QString)"), self._slotSmartQueryChanged)
+        self.connect(self.ui.comboSmartQuery, SIGNAL("editTextChanged(QString)"), self._slotSmartQueryChanged)
+
     def _slotSmartQueryChanged(self):
         smartquery = unicode(self.ui.comboSmartQuery.lineEdit().text())
+        projphaseact, self.hours, self.comment = self._parseSmartQuery(smartquery)
+        if projphaseact != self._projphaseact:
+            self.projects = self.rt.search(projphaseact)
+            self._projphaseact = projphaseact
+        self._smartUpdateGui()
+            
+    def _parseSmartQuery(self, smartquery):
         if smartquery == "":
+            #Se vuota converte in "trova tutto"
             smartquery = "% % %"
-        elif smartquery == self._smartquery:
-            return
-        self._smartquery = smartquery
-        projects = self.rt.search(smartquery)
+        smartdict = libRemoteTimereg.parseSmartQuery(smartquery)
+        projphaseact = "%(project)s %(phase)s %(activity)s" % smartdict
+        return projphaseact, smartdict["hours"], smartdict["comment"]
+        
+    def _smartUpdateGui(self):
         self.ui.comboProjectPhase.clear()
         self.ui.comboActivity.clear()
         projphases = set()
         activities = set()
-        for p in projects:
+        for p in self.projects:
             projphases.add("%(project_name)s / %(phase_name)s" % dict(p.record.items()))
             activities.add(p.record.get("activity_name"))
         self.ui.comboProjectPhase.addItems(list(projphases))
         self.ui.comboActivity.addItems(list(activities))
-        self.ui.labelProjectPhase.setEnabled(self.ui.comboProjectPhase.count() == 1)
-        self.ui.labelActivity.setEnabled(self.ui.comboActivity.count() == 1)
-        if len(projects) == 1:
-            d = self.ui.timeTimeWorked.dateTimeFromText(unicode(projects[0].record.get("hours")))
+        self.ui.labelProjectPhase.setEnabled(self.ui.comboProjectPhase.count() != 1)
+        self.ui.labelActivity.setEnabled(self.ui.comboActivity.count() != 1)
+        if len(self.projects) == 1:
+            d = self.ui.timeTimeWorked.dateTimeFromText(self.hours)
             zero = self.ui.timeTimeWorked.dateTimeFromText("0:00")
             self.ui.timeTimeWorked.setDateTime(d)
             self.ui.labelTimeWorked.setEnabled(d > zero)
-    def _slotTimeregSelected(self):
-        try:
-            selectedid = self.ui.lstTimeregDay.indexFromItem(self.ui.lstTimeregDay.selectedItems()[0]).row()
-        except:
-            selectedid = None
-        if selectedid == None:
-            body = "Select a row..."
-        else:
-            p = self.projects[selectedid]
-            #self.ui.txtbrProjectSummary.setPlainText(repr(p))
-            body = "<h3>%(activitydate)s - %(time)s minutes</h3>%(remark)s" % dict(p.record.items())
-        self.ui.txtbrProjectSummary.setPlainText(body)
-
+            self.ui.labelComment.setEnabled(self.comment != "")
+            self.ui.txtComment.setPlainText(self.comment)
+                
 if __name__ == "__main__":
     app = TimeregApplication(sys.argv)

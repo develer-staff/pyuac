10a12351d95579af8a53a3b33041b8b9a41b47f8
Matteo Bertini <naufraghi@develer.com>
2006-11-14 15:49:37 +0000

    Aggiunte un po' di docstring
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@11406 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/libRemoteTimereg.py
+++ src/libRemoteTimereg.py
@@ -11,30 +11,41 @@
 import urllib, urllib2
 try:
     from xml.etree import ElementTree as ET
-except:
+except ImportError:
     try:
         from elementtree import ElementTree as ET
-    except:
+    except ImportError:
         raise ImportError, "ElementTree (or py2.5) needed"
 
 class RemoteTimereg:
     def __init__(self, achievouri, user, password):
+        """
+        Classe di interfaccia per il modulo Achievo "remote"
+        """
         self.user = user
         self.password = password
         self._achievouri = achievouri
         self._loginurl = urllib.basejoin(self._achievouri, "index.php")
         self._dispatchurl = urllib.basejoin(self._achievouri, "dispatch.php")
         self._login()
+        
     def _login(self, user=None, password=None):
-        if user != None and password != None:
+        """
+        Restituisce il nome utente e rinfresca la sessione di Achievo
+        """
+        if user is not None and password is not None:
             self.user = user
             self.password = password
         auth = urllib.urlencode({"auth_user":self.user,"auth_pw":self.password})
-        self._setup_auth()
+        self._setupAuth()
         #refresh Achievo session
         urllib2.urlopen(self._loginurl, auth).read()
-        return self.urldispatch("whoami")
-    def _setup_auth(self):
+        return self.urlDispatch("whoami")
+    
+    def _setupAuth(self):
+        """
+        Imposta l'autenticazione http e la gestione dei cookies
+        """
         passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
         passman.add_password(None, self._loginurl, self.user, self.password)
         passman.add_password(None, self._dispatchurl, self.user, self.password)
@@ -42,7 +53,11 @@ class RemoteTimereg:
         cookie_handler = urllib2.HTTPCookieProcessor()
         opener = urllib2.build_opener(auth_handler, cookie_handler)
         urllib2.install_opener(opener)
-    def urldispatch(self, node, action="search", **kwargs):
+        
+    def urlDispatch(self, node, action="search", **kwargs):
+        """
+        Invoca il dispatch.php di Achievo
+        """
         params = {"atknodetype": "remote.%s" % node,
                   "atkaction": action}
         #This is the way PHP accepts arrays, without [] it gets only the last value.
@@ -55,18 +70,29 @@ class RemoteTimereg:
             print "########### Dispatch:", qstring
         try:
             return urllib2.urlopen(self._dispatchurl, qstring).read()
-        except:
+        #TODO: scoprire che eccezione solleva...
+        #      oppure no tanto richiamo la stessa funzione subito
+        except: 
             self._login() #Achievo session timeout?
             return urllib2.urlopen(self._dispatchurl, qstring).read()
+        
     def whoami(self):
+        """
+        Restituisce il nome utente della sessione attiva
+        """
         page = self._login()
         elogin = ET.fromstring(page)
         if elogin.find("record").get('name') == self.user:
             return self.user
         else:
             return False
+        
     def search(self, input):
-        page = self.urldispatch("query", input=input)
+        """
+        Ottiene la lista dei progetti/fasi/attività coerenti
+        con la smart-string inviata
+        """
+        page = self.urlDispatch("query", input="%"+input)
         eprojects = ET.fromstring(page)
         res = []
         for project in eprojects:
@@ -74,8 +100,13 @@ class RemoteTimereg:
         if __debug__:
             print res
         return res
-    def timereport(self, date):
-        page = self.urldispatch("timereport", date=date)
+    
+    def timeReport(self, date):
+        """
+        Ottiene la lista delle ore registrate nei giorni
+        passati nel parametro date
+        """
+        page = self.urlDispatch("timereport", date=date)
         etimeregs = ET.fromstring(page)
         res = []
         for timereg in etimeregs:
@@ -85,6 +116,9 @@ class RemoteTimereg:
         return res
     
 class AchievoProject:
+    """
+    Classe di utilità per la smart-search
+    """
     def __init__(self, record):
         self.record = record
     def __repr__(self):
@@ -93,6 +127,11 @@ class AchievoProject:
         return "%(project_name)s (%(phase_name)s) %(activity_name)s" % dict(self.record.items())
 
 class AchievoTimereg:
+    """
+    Classe di utilità per il timeReport
+    """
+    #TODO: Valutare se le due classi sono destinate a differenziarsi
+    #      o se conviene unificarle
     def __init__(self, record):
         self.record = record
     def __repr__(self):
@@ -106,5 +145,5 @@ if __name__ == "__main__":
     rl.search("pr")
     rl.search("pr me")
     rl.search("pr me an")
-    rl.timereport("2006-11-7")
-    rl.timereport(["2006-11-7","2006-11-8"])
+    rl.timeReport("2006-11-7")
+    rl.timeReport(["2006-11-7","2006-11-8"])

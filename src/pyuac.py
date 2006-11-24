d21bf5d315ce7ced33cc6af1ce00c4373a9c5972
Matteo Bertini <naufraghi@develer.com>
2006-11-24 18:48:42 +0000

    Intermezzo lavori di refactoring: * Estratto RemoteTimereg in un file separato. * Aggiunta una funzione unescape per invertire   l'operato di ElementTree su alcuni caratteri (che diventano &#123;). * Connessi i segnali specifici di QRemoteTimereg. * Risolto un bug malefico con la codifica utf-8. TODO: Gestire i segnali di uscita di timereg
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@11447 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/pyuac.py
+++ src/pyuac.py
@@ -8,7 +8,7 @@
 #
 # Author: Matteo Bertini <naufraghi@develer.com>
 
-import sys, time, Queue
+import sys
 from PyQt4 import uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -21,7 +21,40 @@ except ImportError:
     except ImportError:
         raise ImportError, "ElementTree (or py2.5) needed"
 
-import libRemoteTimereg
+from QRemoteTimereg import RemoteTimereg
+
+ZEROtime = QDateTime.fromString("00:00", "HH:mm")
+
+import xml.parsers.expat
+def unescape(s):
+    want_unicode = False
+    if isinstance(s, unicode):
+        s = s.encode("utf-8")
+        want_unicode = True
+
+    # the rest of this assumes that `s` is UTF-8
+    list = []
+
+    # create and initialize a parser object
+    p = xml.parsers.expat.ParserCreate("utf-8")
+    p.buffer_text = True
+    p.returns_unicode = want_unicode
+    p.CharacterDataHandler = list.append
+
+    # parse the data wrapped in a dummy element
+    # (needed so the "document" is well-formed)
+    p.Parse("<e>", 0)
+    p.Parse(s, 0)
+    p.Parse("</e>", 1)
+
+    # join the extracted strings and return
+    es = ""
+    if want_unicode:
+        es = u""
+    return es.join(list)
+
+def debug(msg):
+    qDebug("#-#-#-#-# "+msg.replace(r"%%", r"%").replace(r"%", r"%%"))
 
 class TimeregApplication(QApplication):
     def __init__(self, args):
@@ -45,7 +78,10 @@ class TimeregWindow(QMainWindow):
     def _setupGui(self):
         self.ui.dateTimeregDate.setDate(QDate.currentDate())
         self.ui.comboSmartQuery.lineEdit().setText("")
-        self._slotSmartQueryChanged()
+        self.ui.txtRemark.setPlainText("")
+        self.ui.timeTimeWorked.setDateTime(ZEROtime)
+        self.ui.labelRoundTime.setText("00:00")
+        self._smartQueryChanged()
         #fino a quando non sarà attiva la gestione delle modifiche...
         [i.setEnabled(False) for i in (self.ui.txtRemark,
                                        self.ui.timeTimeWorked,
@@ -54,49 +90,41 @@ class TimeregWindow(QMainWindow):
     def _connectSlots(self):
         self.connect(self.ui.comboSmartQuery,
                      SIGNAL("editTextChanged(QString)"),
-                     self._slotSmartQueryChanged)
-        self.connect(self.rt,
-                     SIGNAL("ready()"),
-                     self._smartUpdateGui)
+                     self._smartQueryChanged)
         self.connect(self.ui.btnSave,
                      SIGNAL("clicked()"),
                      self.timereg)
         self.connect(self.rt,
-                     SIGNAL("timeregging()"),
-                     self._enableGui)
+                     SIGNAL("searchStarted()"),
+                     self._searchStarted)
+        self.connect(self.rt,
+                     SIGNAL("searchDone(PyObject *)"),
+                     self._projectsChanged)
+        self.connect(self.rt,
+                     SIGNAL("timeregStarted()"),
+                     self._timeregStarted)
+        self.connect(self.rt,
+                     SIGNAL("timeregDone()"),
+                     self._timeregDone)
+        self.connect(self.rt,
+                     SIGNAL("timeregError()"),
+                     self._timeregError)
 
-    def _enableGui(self):
-        self.ui.setEnabled(not self.ui.isEnabled())
-        if __debug__:
-            qDebug("_enableGui")
-
-    def _slotSmartQueryChanged(self):
-#        if __debug__:
-#            qDebug("_slotSmartQueryChanged, %s" % self.rt.state())
-        smartquery = self.ui.comboSmartQuery.lineEdit().text()
+    def _smartQueryChanged(self):
+        smartquery = unicode(r"%"+self.ui.comboSmartQuery.lineEdit().text())
         self.rt.search(smartquery)
                      
     def _smartUpdateGui(self):
         if __debug__:
-            qDebug("_smartUpdateGui")
-        msg = self.rt.read()
-
-        if msg == "":
-            if __debug__:
-                qDebug("No Output!")
-            return
-
-        if not self.ui.isEnabled():
-            self._enableGui()
-            self._setupGui()
-
+            debug("_smartUpdateGui")
         self.ui.setWindowTitle("Time Registration - %s" % self.auth[1])
 
-        try:
-            self.projects = ET.fromstring(msg)
-        except libRemoteTimereg.ExpatError:
-            print msg
+    def _projectsChanged(self, projects):
+        if __debug__:
+            debug("_projectsChanged %s" % len(projects))
+        self.projects = projects
 
+        # ---- Update comboboxes ----
         self.ui.comboProjectPhase.clear()
         self.ui.comboActivity.clear()
         projphases = set()
@@ -108,92 +136,53 @@ class TimeregWindow(QMainWindow):
         self.ui.comboActivity.addItems(list(activities))
         self.ui.labelProjectPhase.setEnabled(self.ui.comboProjectPhase.count() != 1)
         self.ui.labelActivity.setEnabled(self.ui.comboActivity.count() != 1)
+        # ^^^^ Update comboboxes ^^^^
 
         if len(self.projects) == 1:
             p = self.projects[0]
-            d = self.ui.timeTimeWorked.dateTimeFromText(p.get("input_hours"))
-            zero = self.ui.timeTimeWorked.dateTimeFromText("00:00")
-            self.ui.timeTimeWorked.setDateTime(d)
+            worked_time = self.ui.timeTimeWorked.dateTimeFromText(p.get("input_hours"))
+            self.ui.timeTimeWorked.setDateTime(worked_time)
             self.ui.labelRoundTime.setText(p.get("hmtime"))
-            self.ui.txtRemark.setPlainText(p.get("remark"))
-            self.ui.labelTimeWorked.setEnabled(d > zero)
+            self.ui.txtRemark.setPlainText(unescape(p.get("remark")))
+            self.ui.labelTimeWorked.setEnabled(worked_time > ZEROtime)
             self.ui.labelRemark.setEnabled(p.get("remark") != "")
-            self.ui.btnSave.setEnabled(d > zero and p.get("remark") != "")
+            self.ui.btnSave.setEnabled(worked_time > ZEROtime and p.get("remark") != "")
         else:
             self.ui.txtRemark.setPlainText("")
             self.ui.labelTimeWorked.setEnabled(False)
             self.ui.labelRemark.setEnabled(False)
             self.ui.btnSave.setEnabled(False)
  
+    def _timeregStarted(self):
+        if __debug__:
+            debug("_timeregStarted")
+    
+    def _timeregDone(self):
+        if __debug__:
+            debug("_timeregDone")
+
+    def _timeregError(self):
+        if __debug__:
+            debug("_timeregError")
+
+    def _searchStarted(self):
+        if __debug__:
+            debug("_searchStarted")
+     
     def timereg(self):
+        #if __debug__:
+        #    debug("timereg" % (ET.tostring(self.projects[0])))
+        debug("before project")
         p = self.projects[0]
+        debug("after project")
         params = dict([(k, p.get(k)) for k in "projectid phaseid activityid hmtime remark".split()])
+        debug("timereg2")
         params["activitydate"] = self.ui.dateTimeregDate.date().toString("yyyyMMdd")
+        if __debug__:
+            debug(str(params))
         self.rt.timereg(**params)
         self.ui.setWindowTitle("Time Registration - saving...")
 
-
-class RemoteTimereg(QObject):
-    """
-    Classe per la gesitione asincrona della libreria RemoteAchievo
-    """
-    def __init__(self, parent, auth):
-        QObject.__init__(self, parent)
-        self.process = QProcess(self)
-        self.timer = QTimer(self)
-        self.auth = auth
-        self.queue = ""
-        self._noops = 0
-        self._oops = 0
-        self.connect(self.timer, SIGNAL("timeout()"), self._sync)
-        self.connect(self.process, SIGNAL("finished(int)"), self._ready)
-
-    def read(self):
-        return str(self.process.readAllStandardOutput()).decode("UTF-8")
-
-    def search(self, msg):
-        if __debug__:
-            qDebug("Query")
-        self.queue = "search?smartquery=%" + str(msg).encode("UTF-8") + "\n"
-        if not self.timer.isActive():
-            self._sync()
-            self.timer.start(100)
-
-    def _sync(self, timerEvent=None):
-        if __debug__:
-            qDebug("sync")
-        if self.process.state() == self.process.NotRunning:
-            if self.queue != "":
-                self.process.start("./pyuac_cli.py", self.auth+["--"])
-                self.process.write(self.queue)
-                self.queue = ""
-            else:
-                #dopo N loop senza modifiche, ferma il timer
-                self._noops += 1
-                if self._noops > 5:
-                    self.timer.stop()
-                    self._noops = 0
-        else:
-            #dopo NN loop di attesa remina il processo
-            self._oops += 1
-            if self._oops > 500:
-                self.process.terminate()
-                self._oops = 0
- 
-    def _ready(self):
-        self.emit(SIGNAL("ready()"))
-
-    def timereg(self, **kwargs):
-        if __debug__:
-            qDebug("Saving")
-        self.queue = ""
-        qstring = libRemoteTimereg.urllib.urlencode(kwargs).encode("UTF-8")
-        if __debug__:
-            qDebug(qstring)
-        self.process.start("./pyuac_cli.py", self.auth+["--"])
-        self.process.write("timereg?" + qstring + "\n")
-        self.emit(SIGNAL("timeregging()"))
-
 if __name__ == "__main__":
     app = TimeregApplication(sys.argv)
-    #2 m a 2:34 prova prova
+    #2 m a 2:34 prova prova èàò

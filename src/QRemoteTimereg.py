97a08fe72d63be4848fb2644c13b738834c3bca3
Matteo Bertini <naufraghi@develer.com>
2006-12-01 15:03:41 +0000

    Automatizzata la delegazione dei metodi alla cli e la generazione dei segnali di risposta.
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@11511 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/QRemoteTimereg.py
+++ src/QRemoteTimereg.py
@@ -13,32 +13,33 @@ from PyQt4 import uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-import libRemoteTimereg
+try:
+    from xml.etree import ElementTree as ET
+except ImportError:
+    try:
+        from elementtree import ElementTree as ET
+    except ImportError:
+        raise ImportError, "ElementTree (or py2.5) needed"
+
+import libRemoteTimereg, pyuac_cli
 log = logging.getLogger("pyuac.gui")
 
 def debug(msg):
     if __debug__:
-        qDebug("#-#-#-#-# "+msg.replace(r"%%", r"%").replace(r"%", r"%%"))
+        log.debug(msg)
+        print __name__, msg
 
 class RemoteTimereg(QObject):
     """
     Classe per la gestione asincrona della libreria RemoteAchievo
-    La classe espone due metodi:
-    * search(smartquery)
-      che emette searchStarted() e searchDone(PyObject *)
-      searchDone restituiesce un oggetto ElementTree
-    * timereg(**kwargs)
-      accetta un dizionario di valori necessari per registrare
-      un blocco di ore lavorate
-      emette timeregStarted() e timeregDone()
+    La classe è mappata direttamente sui metodi di RemoteAchievo
+    tramite codifica urlencode per la chiamata ed xml per la risposta
     """
     def __init__(self, parent, auth):
         QObject.__init__(self, parent)
         self.process = QProcess(self)
         self.auth = auth
-        self._query = ""
-        self._timereg = ""
-        self._timereport = ""
+        self._actions_params = {}
         self.connect(self.process, SIGNAL("finished(int)"), self._ready)
         self.connect(self.process, SIGNAL("error(QProcess::ProcessError)"),
                      self._error)
@@ -55,7 +56,7 @@ class RemoteTimereg(QObject):
         for k, v in kwargs.items():
             kwargs[k] = unicode(v).strip().encode("utf-8") #se v è un QString
         qstring = urllib.urlencode(kwargs, doseq=True)
-        debug("**"+qstring)
+        debug("_encode "+qstring)
         return action + "?" + qstring
 
     def _execute(self, qstring):
@@ -70,67 +71,64 @@ class RemoteTimereg(QObject):
         else:
             return False
 
-    def search(self, query):
-        debug("Search")
-        self._query = self._encode("search", smartquery=query)
-        self._sync()
-
-    def timereg(self, **kwargs):
-        debug("Timereg")
-        self._timereg = self._encode("timereg", **kwargs)
-        self._sync()
-
-    def timereport(self, **kwargs):
-        debug("Timereport")
-        self._timereport = self._encode("timereport", **kwargs)
-        self._sync()
+    def __getattr__(self, action):
+        debug(action)
+        if action in libRemoteTimereg.RemoteTimereg.actions:
+            def _action(**kwargs):
+                self._actions_params[action] = self._encode(action, **kwargs)
+                self._sync()
+            return _action
+        else:
+            raise AttributeError
 
     def _sync(self):
         """
         Provvede ad eseguire le query in attesa
-        ed emette i segnali adatti alla query avviata
+        ed emette i segnali adatti alla query avviata:
+            whoamiStarted
+            queryStarted
+            timeregStarted
+            timereportStarted
         """
         debug("Sync")
-        if self._query != "" and self._execute(self._query):
-            self._query = ""
-            self.emit(SIGNAL("searchStarted()"))
-        elif self._timereg != "" and self._execute(self._timereg):
-            self._timereg = ""
-            self.emit(SIGNAL("timeregStarted()"))
-        elif self._timereport != "" and self._execute(self._timereport):
-            self._timereport = ""
-            self.emit(SIGNAL("timereportStarted()"))
+        for action, cmdline in self._actions_params.items():
+            if self._execute(cmdline):
+                del self._actions_params[action]
+                self.emit(SIGNAL(action+"Started"))
 
     def _ready(self, exitcode):
         """
-        Provvede a emettere i segnali adatti alla risposta ottenuta
+        Provvede a emettere i segnali adatti alla risposta ottenuta:
+            whoami[OK|Err](ElemetTree)
+            query[OK|Err](ElemetTree)
+            timereg[OK|Err](ElemetTree)
+            timereport[OK|Err](ElemetTree)
+            emptyResponse
         """
         debug("Ready")
+        if exitcode != pyuac_cli.exits.index("OK"):
+            self._error(5, exitcode)
         resp = str(self.process.readAllStandardOutput())
-        if exitcode != 0:
-            self._error(exitcode)
-        if resp == "":
-            return
-        eresp = libRemoteTimereg.msgParse(resp)
-        print "eresp id", id(eresp)
-        node = eresp.get("node")
-        msg = eresp.get("msg")
-        if node == "query":
-            debug("pre emit searchDone")
-            self.emit(SIGNAL("searchDone"), eresp)
-            debug("post emit searchDone")
-        elif node == "timereg" and msg == "OK":
-            self.emit(SIGNAL("timeregDone()"))
-        elif node == "timereg" and msg == "Err":
-            self.emit(SIGNAL("timeregError()"))
-        elif node == "timereport":
-            self.emit(SIGNAL("timereportDone"), eresp)
+        if resp != "":
+            eresp = ET.fromstring(resp)
+            node = eresp.get("node")
+            msg = eresp.get("msg")
+            self.emit(SIGNAL(node+msg), eresp)
         else:
-            pass
+            self.emit(SIGNAL("emptyResponse"))
         self._sync()
 
-    def _error(self, error):
-        debug("Err: "+str(self.process.readAllStandardError()))
-        self.emit(SIGNAL("processError(int)"), error)
+    def _error(self, qerror, exitcode=None):
+        """
+        Emette processError con parametri:
+            QProcess::ProcessError decodificato come stringa
+            pyuac_cli.error decodificato come stringa
+        """
+        qerrors = "FailedToStart Crashed Timedout WriteError ReadError UnknownError"
+        qerror = qerrors.split()[qerror]
+        debug("Err(%s): %s" % (qerror, str(self.process.readAllStandardError())))
+        if exitcode != None:
+            exitcode = pyuac_cli.exits[exitcode]
+        self.emit(SIGNAL("processError"), qerror, exitcode)
 
 

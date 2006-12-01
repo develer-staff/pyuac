40ad4df8ff603e5926bb3abf4e130476586ad908
Matteo Bertini <naufraghi@develer.com>
2006-12-01 11:02:09 +0000

    Spostate le *action" exposte nella libreria Smontato while True in tante chiamate a funzioni di aiuto Creata una codifica per i codici di errore
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@11500 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/pyuac_cli.py
+++ src/pyuac_cli.py
@@ -8,14 +8,18 @@
 #
 # Author: Matteo Bertini <naufraghi@develer.com>
 
-import sys, cgi, logging
+import sys, cgi, logging, urllib2
 import libRemoteTimereg
 
-ET = libRemoteTimereg.ET
+try:
+    from xml.etree import ElementTree as ET
+except ImportError:
+    try:
+        from elementtree import ElementTree as ET
+    except ImportError:
+        raise ImportError, "ElementTree (or py2.5) needed"
 
 log = logging.getLogger("pyuac.cli")
-
-out = sys.stdout.write
 
 help = """Uso:
     http://domain.com/achievo/ user password [--]
@@ -33,6 +37,48 @@ def checkParams(params):
         #almeno 4, parte in modalità oneshot
         return sys.argv[1:4], True
 
+def parseCommand(cmdline):
+    action_params = cmdline.split("?", 1)
+    action = action_params[0]
+    params = {}
+    if len(action_params) > 1:
+        for k, v in cgi.parse_qsl(action_params[1]):
+            if len(v) == 1:
+                # parse_qsl restituisce sempre array (anche singole)
+                params[str(k)] = v[0]
+                # str(k) perchè poi le chiavi del dizionario passeranno
+                # per getattr che non accetta unicode (sono nomi di metodi)
+            else:
+                # devo comunque convertire in stringa il nome (orig. unicode)
+                params[str(k)] = v
+    if __debug__:
+        log.debug("<!--cli params: \n%s\n-->\n" % str(params))
+    return action, params
+
+def help(lib):
+    res = ["Usare una delle azioni definite:"]
+    res += ["  q: Quit"]
+    res += ["  %s: %s" % (action, lib.actions[action]) for lib.action in actions]
+    return "\n".join(res)
+
+exits = "OK PARAMS_ERROR CONNECTION_ERROR ACTION_ERROR RESPONSE_ERROR".split()
+def exit(mode):
+    sys.exit(exits.index(mode))
+
+def execute(lib, action, params):
+    if __debug__:
+        log.debug("cli.%s(%s)" % (action, params))
+    #Cerco di mappare l'azione su un metodo
+    func = getattr(lib, action)
+    if params:
+        eres = func(**params)
+    else:
+        eres = func()
+    res = ET.tostring(eres, "utf-8")
+    if __debug__:
+        log.debug("cli.%s(%s) results: %s" % (action, params, res))
+    return res
+
 def serve(params, oneshot=False):
     """
     Questa funzione aspetta l'input dell'utente in forma
@@ -41,77 +87,34 @@ def serve(params, oneshot=False):
     sul metodo *action* di RemoteTimereg, se questo è
     presente nel dizionario di azioni permesse (e documentate)
     """
-    actions = {"q": "Quit",
-               "search": "Search the project",
-               "whoami": "Returns login info",
-               "timereg": "Register worked time",
-               "timereport": "Report time registered in the provided date"
-              }
     try:
-        rt = libRemoteTimereg.RemoteTimereg(*params)
-    except libRemoteTimereg.urllib2.HTTPError:
+        #Cerca di inizializzare la classe con i parametri forniti
+        lib = libRemoteTimereg.RemoteTimereg(*params)
+    except urllib2.HTTPError:
         log.error("Connection Error!!\n")
-        sys.exit(1)
+        exit("CONNECTION_ERROR")
+
     while True:
         #Gira aspettando righe di comando della forma:
         # action?url_encoded=params&other=params
         prompt = (not oneshot) and "remote: " or ""
-        msg = raw_input(prompt).strip()
-        cmdline = msg.split("?", 1)
-        action = cmdline[0]
-        params = {}
-        if len(cmdline) > 1:
-            for k, v in cgi.parse_qsl(cmdline[1]):
-                if len(v) == 1:
-                    #parse_qsl restituisce sempre array (anche singole)
-                    params[str(k)] = v[0]
-                else:
-                    #devo comunque convertire in stringa il nome (orig. unicode)
-                    params[str(k)] = v
-        if __debug__:
-            log.debug("<!--cli params: \n%s\n-->\n" % str(params))
-        if action not in actions:
-            if not oneshot:
-                print "Usare una delle azioni definite:"
-                for action in actions:
-                    print "  %s: %s" % (action, actions[action])
-            else: # if oneshot
-                sys.exit(1)
-        else: #action in actions
-            if action == "q":
-                sys.exit(0)
-            else:
-                #Cerco di mappare l'azione su un metodo
-                if __debug__:
-                    log.debug("cli.%s(%s)" % (action, params))
-                try:
-                    func = getattr(rt, action)
-                except:
-                    log.error("Attribute Error! %s(%s)\n" % (action, params))
-                    sys.exit(1)
-                try:
-                    if params:
-                        eres = func(**params)
-                    else:
-                        eres = func()
-                except:
-                    log.error("Call Error! %s(%s)\n" % (action, params))
-                    sys.exit(1)
-                try:
-                    res = libRemoteTimereg.emsgDump(eres)
-                except:
-                    log.error("Response Error! %s(%s)\n" % (action, params))
-                    sys.exit(1)
-                if __debug__:
-                    log.debug("cli.%s(%s) results: %s" % (action, params, res))
-                out(res+"\n")
-                if oneshot:
-                    sys.exit(0)
+        cmdline = raw_input(prompt).strip()
+        action, params = parseCommand(cmdline)
+        if action in lib.actions:
+            print execute(lib, action, params)
+            if oneshot:
+                exit("OK")
+        elif action == "q":
+            exit("OK")
+        elif not oneshot:
+            print help()
+        else:
+            exit("ACTION_ERROR")
 
 if __name__=="__main__":
     params, oneshot = checkParams(sys.argv[1:])
     if params:
         serve(params, oneshot=oneshot)
     else:
-        out(help)
-        sys.exit(1)
+        print help
+        exit("PARAMS_ERROR")

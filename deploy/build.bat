80756cd89aea34913c13186df67b071b9adcd88f
Lorenzo Mancini <lmancini@develer.com>
2006-12-21 16:57:19 +0000

    Script per build one-dir
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@11667 38d2e660-2303-0410-9eaa-f027e97ec537

--- deploy/build.bat
+++ deploy/build.bat
@@ -1,7 +1,9 @@
-rmdir /S /Q buildpyuac_onefile
-del warnpyuac_onefile.txt
+rmdir /S /Q distpyuac_onedir
+del warndistpyuac_onedir.txt
+del pyuac.exe
+del pyuac_cli.exe
 
 python pyinstaller/configure.py
-python pyinstaller/Build.py pyuac_onefile.spec
+python pyinstaller/Build.py pyuac_onedir.spec
 
 rem "c:\Programmi\Inno Setup 5\ISCC.exe" pyuac.iss

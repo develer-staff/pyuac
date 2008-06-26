f46c923c447199230fd05cac6cc0ed78f4f3c227
Lorenzo Berni <duplo@develer.com>
2008-06-26 14:46:13 +0000

    Corretto errore di digitazione (parte2).
    git-svn-id: https://src.develer.com/svn/pyuac/branches/vista_settimanale@21864 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/QTimeBrowseWindow.py
+++ src/QTimeBrowseWindow.py
@@ -60,8 +60,8 @@ class TimeBrowseWindow(QMainWindow, QAchievoWindow):
 
     def __auth__(self, auth):
         self.__setup__(auth, 'pyuac_browse.ui')
-        self._setupGui()
         self._mode= ""
+        self._setupGui()
         self._connectSlots()
         self.ui.show()
 

c07ef3f897e3330aa1af0e71e9e747744c57a38b
Lorenzo Berni <duplo@develer.com>
2008-07-30 12:22:44 +0000

    Il segnale viene emesso solamente nel caso la richiesta non sia stata abortita.
    git-svn-id: https://src.develer.com/svn/pyuac/trunk@22195 38d2e660-2303-0410-9eaa-f027e97ec537

--- src/QRemoteTimereg.py
+++ src/QRemoteTimereg.py
@@ -258,9 +258,10 @@ class QRemoteTimereg(QObject):
             self._response.append(eresp)
             #print "QRemoteTimereg risposta accodata"
         #emette un segnale indicando lo stato di avanzamento della richiesta.
-        self.emit(SIGNAL("progress"),
-                  float(self._current_action[1]) /
-                  len(self._pending_requests[self._current_action[0]]))
+        if self._current_action:
+            self.emit(SIGNAL("progress"),
+                      float(self._current_action[1]) /
+                      len(self._pending_requests[self._current_action[0]]))
         #cancella la variabile contenente la risposta
         self._resp = ""
         self._execute()
